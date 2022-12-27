cf00734beea713fe590c89ad5ce70ee3da49097d
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-27 19:26:11 +0530

    Check DailyWage

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -11,43 +11,29 @@ namespace EmployeewageProblem
     {
         public static void CheckAttendence()
         {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
+            int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
-            const int NUM_OF_WORKING_DAYS = 20;
 
             int empHrs = 0;
             int empWage = 0;
-            int totalEmpWage = 0;
 
-            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+
+            if (empCheck==IS_FULL_TIME)
             {
-
-
-                Random random = new Random();
-                int empCheck = random.Next(3);
-
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-
-
-                }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-
-                Console.WriteLine("The Employee Wage " + empWage);
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
-            Console.WriteLine("Total Employee Wage " + totalEmpWage);
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
 
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Emp  Wage " + empWage);
         }
     }
 }
