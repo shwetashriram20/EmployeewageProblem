af1a53ce994fe0d0a132be7c1f8c17ecda4a9244
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-27 19:57:31 +0530

    Calculating TotalWages

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -14,29 +14,40 @@ namespace EmployeewageProblem
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAYS = 20;
+
 
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(3);
 
-            switch (empCheck)
+            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                        empWage = empHrs * EMP_RATE_PER_HOUR;
+
+                        Console.WriteLine("The Employee  Wage " + empWage);
+                }
+                Console.WriteLine("Total Employee Wage " + totalEmpWage);
 
 
-                    empWage = empHrs * EMP_RATE_PER_HOUR;
-
-                    Console.WriteLine("The Employee  Wage " + empWage);
+            }
         }
-    }
 }
