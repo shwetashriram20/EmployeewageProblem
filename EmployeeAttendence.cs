34a275249747ec96ddb00ff10d2081fb6f4fa50c
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-27 19:39:29 +0530

    Add Parttime

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -11,28 +11,35 @@ namespace EmployeewageProblem
     {
         public static void CheckAttendence()
         {
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 10;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
 
-            if (empCheck==IS_FULL_TIME)
+            if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
                 empHrs = 0;
-            }
 
-                empWage = empHrs * EMP_RATE_PER_HOUR;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
                 Console.WriteLine("Emp  Wage " + empWage);
         }
     }
